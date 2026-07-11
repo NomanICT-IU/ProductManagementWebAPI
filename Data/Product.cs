f30134690a235165abf9b6d99369d39f7941b9f7
Abdullah Al Noman <noman.ict.iu@gmail.com>
2026-07-12 01:55:37 +0600

    <feat> Model validation has been added

--- Data/Product.cs
+++ Data/Product.cs
@@ -10,8 +10,10 @@ namespace ProductManagementWebAPI.Data
         [Required]
         public string? Description { get; set; }
         [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0.")]
         public decimal Price { get; set; }
         [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be greater than 0.")]
         public int Quantity { get; set; }
         public DateTime CreatedDate { get; set; }
 
