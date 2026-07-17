56c5524d53d964a0918c6579ed5719de73546578
Abdullah Al Noman <noman.ict.iu@gmail.com>
2026-07-17 11:36:37 +0600

    <feat>add productId support to SendNotification hub method

--- Models/NotificationHub.cs
+++ Models/NotificationHub.cs
@@ -4,9 +4,9 @@ namespace ProductManagementWebAPI.Models
 {
     public class NotificationHub : Hub
     {
-        public async Task SendNotification(string user, string message)
+        public async Task SendNotification(string user, string message, int? productId = null)
         {
-            await Clients.Others.SendAsync("ReceivedNotification", user, message);
+            await Clients.Others.SendAsync("ReceivedNotification", user, message, productId);
         }
     }
 }
